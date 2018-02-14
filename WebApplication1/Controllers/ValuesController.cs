7bc62623ba5d2ffacde41e69a320c420d3942310
Dave Tillman <dtillman@pivotal.io>
2018-02-14 14:22:18 -0700

    Initial checkin

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this._logger = logger;
+        }
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("GET api/values");
+            return "From Get";
+        }
+
+        // GET api/values
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Post()
+        {
+            _logger.LogInformation("POST api/values");
+            string posted = await GetPostData();
+            return posted;
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        private async Task<string> GetPostData()
+        {
+
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Common.Discovery;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        ILogger<ValuesController> _logger;
+        DiscoveryHttpClientHandler _handler;
+        public ValuesController(IDiscoveryClient client, ILogger<ValuesController> logger)
+        {
+            _handler = new DiscoveryHttpClientHandler(client);
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            Uri uri = new Uri(@"https://FORTUNESERVICE/api/values");
+            var client = new HttpClient(_handler, false);
+            var getOutput = await client.GetStringAsync(uri);
+            return getOutput;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Post()
+        {
+
+            Uri uri = new Uri(@"https://FORTUNESERVICE/api/values");
+            var client = new HttpClient(_handler, false);
+            try
+            {
+                string posted = await GetPostData();
+                var result = await client.PostAsync(uri, new StringContent(posted));
+                var strg = await result.Content.ReadAsStringAsync();
+                return strg;
+
+            } catch(Exception e)
+            {
+                _logger.LogInformation(e, "Failed");
+            }
+            return "Failed";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        private async Task<string> GetPostData()
+        {
+
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
